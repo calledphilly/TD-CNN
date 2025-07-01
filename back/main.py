32b5194027b89e942ea4608e08418caff7476e57
calledphilly <ybikouta@icloud.com>
2025-07-01 17:39:23 +0200

    Initial commit

---
+++ back/main.py
@@ -0,0 +1,68 @@
+import base64
+import io
+import cv2
+import numpy as np
+import onnxruntime as ort
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from PIL import Image
+from pydantic import BaseModel
+
+app = FastAPI()
+session = ort.InferenceSession("mnist_cnn2.onnx")
+
+
+class ImagePayload(BaseModel):
+    image: str
+
+
+@app.post("/predict")
+async def predict(payload: ImagePayload):
+    image_b64 = payload.image.split(",")[1]
+    image_bytes = base64.b64decode(image_b64)
+    img = Image.open(io.BytesIO(image_bytes)).convert("L")
+    img_np = np.array(img)
+    img_np = 255 - img_np  # inversion
+    _, img_bin = cv2.threshold(img_np, 10, 255, cv2.THRESH_BINARY)
+    coords = cv2.findNonZero(img_bin)
+    x, y, w, h = cv2.boundingRect(coords)
+    digit = img_np[y:y+h, x:x+w]
+    
+    # digit_resized = cv2.resize(digit, (20, 20), interpolation=cv2.INTER_AREA)
+    h, w = digit.shape
+    if h > w:
+        new_h = 20
+        new_w = int(w * (20.0 / h))
+    else:
+        new_w = 20
+        new_h = int(h * (20.0 / w))
+    digit_resized = cv2.resize(digit, (new_w, new_h), interpolation=cv2.INTER_AREA)
+
+    canvas = np.zeros((28, 28), dtype=np.uint8)
+    x_offset = (28 - new_w) // 2
+    y_offset = (28 - new_h) // 2
+    canvas[y_offset:y_offset+new_h, x_offset:x_offset+new_w] = digit_resized
+    
+    debug_img = Image.fromarray(canvas)
+    debug_img.save("../debug_digit.png")
+    
+    img_norm = canvas.astype(np.float32) / 255.0
+    img_norm = (img_norm - 0.1307) / 0.3081
+
+    input_tensor = img_norm.reshape(1, 1, 28, 28)
+    outputs = session.run(None, {"input": input_tensor})
+    prediction = int(np.argmax(outputs[0]))
+    
+    print("Input shape:", input_tensor.shape)
+    print("Min:", input_tensor.min(), "Max:", input_tensor.max())
+    
+    return {"prediction": prediction}
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["POST"],
+    allow_headers=["*"],
+)
---
+++ front/src/app/page.tsx
@@ -0,0 +1,76 @@
+'use client';
+import { useRef, useState } from 'react';
+export default function Home() {
+	const canvasRef = useRef<HTMLCanvasElement>(null);
+	const [isDrawing, setIsDrawing] = useState(false);
+
+	const startDraw = (e: React.MouseEvent) => {
+		const ctx = canvasRef.current?.getContext('2d');
+		if (!ctx) return;
+		ctx.beginPath();
+		ctx.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+		setIsDrawing(true);
+	};
+
+	const draw = (e: React.MouseEvent) => {
+		if (!isDrawing || !canvasRef.current) return;
+		const ctx = canvasRef.current.getContext('2d');
+		if (!ctx) return;
+		ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+		ctx.strokeStyle = 'black';
+		ctx.lineWidth = 20;
+		ctx.lineCap = 'round';
+		ctx.stroke();
+	};
+
+	const endDraw = () => {
+		setIsDrawing(false);
+	};
+	const clearCanvas = () => {
+		const canvas = canvasRef.current;
+		if (!canvas) return;
+		const ctx = canvas.getContext('2d');
+		if (!ctx) return;
+		ctx.fillStyle = 'white'; // couleur de fond
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
+	};
+	return (
+		<div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+			<main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
+				<canvas
+					ref={canvasRef}
+					width={280}
+					height={280}
+					className='border bg-white'
+					onMouseDown={startDraw}
+					onMouseMove={draw}
+					onMouseUp={endDraw}
+					onMouseLeave={endDraw}
+				/>
+				<div className='flex flex-col justify-center w-full'>
+					<button
+						className='bg-white text-black px-4 py-2 rounded-xl'
+						onClick={clearCanvas}>
+						Réinitialiser
+					</button>
+					<button
+						className='mt-4 bg-white text-black px-4 py-2 rounded-xl'
+						onClick={async () => {
+							const canvas = canvasRef.current;
+							if (!canvas) return;
+							const imageData = canvas.toDataURL('image/png');
+							const res = await fetch('http://localhost:8000/predict', {
+								method: 'POST',
+								headers: { 'Content-Type': 'application/json' },
+								body: JSON.stringify({ image: imageData }),
+							});
+							const data = await res.json();
+							alert(`Prédiction : ${data.prediction}`);
+						}}>
+						Prédire
+					</button>
+				</div>
+			</main>
+		</div>
+	);
+}
